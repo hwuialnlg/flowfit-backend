11fce548714c3802ad179c076e9b4174aca8faa3
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-09-19 23:32:54 -0700

    modify

--- app/database.py
+++ app/database.py
@@ -7,7 +7,11 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
-engine = create_engine(f'postgresql+psycopg2://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_SERVER")}:5432/postgres', echo=True)
+if (os.getenv("TYPE") == "DOCKER"):
+    engine = create_engine(f'{os.getenv("DB_TYPE")}+{os.getenv("DB_DRIVER")}://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DOCKER_LINK")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}', echo=True)
+else:
+    engine = create_engine(f'{os.getenv("DB_TYPE")}+{os.getenv("DB_DRIVER")}://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_SERVER")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}', echo=True)
+
 
 # if not using docker replace environment variables
 # also, replace the database type + database API driver + last name
