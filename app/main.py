14e34c57844a4439558fe8d294c46a03eeb41830
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-09-30 23:17:50 -0700

    cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 
 # routers
@@ -7,6 +8,21 @@ from app.endpoints.user_endpoints import router as user_router
 
 Base.metadata.create_all(engine)
 app = FastAPI()
+
+# add other origins here / cors
+# domain name of frontend needs to be added eventually....
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # include other routers here
 app.include_router(user_router)
