100604eebd7a19ee99020c73bbd936f7de9595ca
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-10-01 22:45:21 -0700

    Update User.py

--- app/db_models/User.py
+++ app/db_models/User.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Date, LargeBinary
 from app.db_models.BaseModel import BaseModel
 
 class User(BaseModel):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     username = Column(String)
     created_at = Column(Date)
