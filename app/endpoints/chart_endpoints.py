0312fab3702d829ec2d32bfd47c7207463b60427
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-12-29 16:30:29 -0800

    calculate labels and provide

--- app/endpoints/chart_endpoints.py
+++ app/endpoints/chart_endpoints.py
@@ -1,4 +1,5 @@
 import datetime
+import calendar
 from dateutil.relativedelta import relativedelta
 from fastapi import APIRouter, HTTPException
 from fastapi import Depends
@@ -25,16 +26,44 @@ async def get_chart(exercise_id: int, time: str = "1M", current_user: dict = Dep
         res = db.query(Exercise).filter(sqlalchemy.and_(Exercise.email == current_user.get("email"), Exercise.id == exercise_id)).one()
         end_range = datetime.datetime.now()
         start_range = None
+        labels = []
         if (time == "ALL"):
-            return {"stats": [stat.toDict() for stat in res.exercisestats]}
+            labels = list({i.date.year for i in res.exercisestats})
+            return {"stats": [stat.toDict() for stat in res.exercisestats], "labels": labels}
         elif (time == "1M"):
+            # need to cleanup returned calendar.monthrange stuff
             start_range = end_range - relativedelta(months=1)
+            if start_range.month == end_range.month:
+                labels = [i + 1 for i in range(calendar.monthrange(datetime.datetime.now().year, start_range.month)[1])]
+            else:
+                # iterate to get previous months dates
+                while start_range.day <= calendar.monthrange(start_range.year, start_range.month)[1]:
+                    labels.append(f"{start_range.year}-{start_range.month}-{start_range.day}")
+                    if start_range.day != 31:
+                        start_range += relativedelta(days=1)
+                    else:
+                        break
+                labels.extend([f"{end_range.year}-{end_range.month}-{i+1}" for i in range(calendar.monthrange(end_range.year, end_range.month)[1]) if i + 1 <= end_range.day])
         elif (time == "3M"):
-            start_range = end_range - relativedelta(months=3)
+            start_range = end_range - relativedelta(months=2)
+            if start_range.year == end_range.year:
+                labels = [f"{end_range.year}-{i+1}" for i in range(end_range.month) if start_range.month <= i+1 <= end_range.month]
+            else:
+                while start_range.month <= 12:
+                    labels.append(f"{start_range.year}-{start_range.month}")
+                    start_range += datetime.timedelta(months=1)
+                labels.extend([f"{end_range.year}-{i+1}" for i in range(end_range.month)])
         elif (time == "1Y"):
             start_range = end_range - relativedelta(years=1)
+            if start_range.year == end_range.year:
+                labels = [f"{start_range.year}-{i+1}" for i in range(12)]
+            else:
+                while start_range.month <= 12:
+                    labels.append(f"{start_range.year}-{start_range.month}")
+                    start_range += datetime.timedelta(months=1)
+                labels.extend([f"{end_range.year}-{i+1}" for i in range(end_range.month)])
 
-        return {"stats" : [stat.toDict() for stat in res.exercisestats if start_range.date() <= stat.date <= end_range.date()]}
+        return {"stats" : [stat.toDict() for stat in res.exercisestats if start_range.date() <= stat.date <= end_range.date()], "labels": labels}
 
     except Exception as e:
         raise HTTPException(status_code=404, detail=e)
