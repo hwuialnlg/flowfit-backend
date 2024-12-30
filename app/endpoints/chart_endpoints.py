7b5ea029eb89339fe07f920897b9177120ca9d5a
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-12-29 18:04:36 -0800

    fixes

--- app/endpoints/chart_endpoints.py
+++ app/endpoints/chart_endpoints.py
@@ -26,6 +26,7 @@ async def get_chart(exercise_id: int, time: str = "1M", current_user: dict = Dep
         res = db.query(Exercise).filter(sqlalchemy.and_(Exercise.email == current_user.get("email"), Exercise.id == exercise_id)).one()
         end_range = datetime.datetime.now()
         start_range = None
+        copy_start_range = None
         labels = []
         if (time == "ALL"):
             labels = list({i.date.year for i in res.exercisestats})
@@ -33,6 +34,7 @@ async def get_chart(exercise_id: int, time: str = "1M", current_user: dict = Dep
         elif (time == "1M"):
             # need to cleanup returned calendar.monthrange stuff
             start_range = end_range - relativedelta(months=1)
+            copy_start_range = start_range
             if start_range.month == end_range.month:
                 labels = [i + 1 for i in range(calendar.monthrange(datetime.datetime.now().year, start_range.month)[1])]
             else:
@@ -46,6 +48,7 @@ async def get_chart(exercise_id: int, time: str = "1M", current_user: dict = Dep
                 labels.extend([f"{end_range.year}-{end_range.month}-{i+1}" for i in range(calendar.monthrange(end_range.year, end_range.month)[1]) if i + 1 <= end_range.day])
         elif (time == "3M"):
             start_range = end_range - relativedelta(months=2)
+            copy_start_range = start_range
             if start_range.year == end_range.year:
                 labels = [f"{end_range.year}-{i+1}" for i in range(end_range.month) if start_range.month <= i+1 <= end_range.month]
             else:
@@ -55,6 +58,7 @@ async def get_chart(exercise_id: int, time: str = "1M", current_user: dict = Dep
                 labels.extend([f"{end_range.year}-{i+1}" for i in range(end_range.month)])
         elif (time == "1Y"):
             start_range = end_range - relativedelta(years=1)
+            copy_start_range = start_range
             if start_range.year == end_range.year:
                 labels = [f"{start_range.year}-{i+1}" for i in range(12)]
             else:
@@ -63,7 +67,7 @@ async def get_chart(exercise_id: int, time: str = "1M", current_user: dict = Dep
                     start_range += datetime.timedelta(months=1)
                 labels.extend([f"{end_range.year}-{i+1}" for i in range(end_range.month)])
 
-        return {"stats" : [stat.toDict() for stat in res.exercisestats if start_range.date() <= stat.date <= end_range.date()], "labels": labels}
+        return {"stats" : [stat.toDict() for stat in res.exercisestats if copy_start_range.date() <= stat.date <= end_range.date()], "labels": labels}
 
     except Exception as e:
         raise HTTPException(status_code=404, detail=e)
